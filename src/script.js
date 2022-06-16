a58ea47115eca1e6d5178ed89f776a790d9d0fdf
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-06-15 21:45:47 -0400

    fixed functions

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ let num1 = document.getElementById('num1');
 let respostaCerta = null;
 let random1 = null;
 let random2 = null;
+let controle = 20;
 
 function getRandomNumber(max, min) {
   return Math.floor(Math.random() * (max -min)) + min;
@@ -13,6 +14,14 @@ function newRandomNumber() {
   num1.textContent = random1;
   random2 = getRandomNumber(random1, 30);
   num2.textContent = random2;
+  if (controle <= 10 && controle > 0) {
+    let acao = document.getElementById('acao');
+    acao.textContent = "-";
+  } else if (controle <= 0) {
+    window.location.href = "estatisticas.html";
+  }
+  controle--
+  
 }
 
 let answer = document.getElementById('resposta');
@@ -34,7 +43,9 @@ function checkAnswer() {
 
   let resposta = answer.value;
   resposta = parseFloat(resposta);
-  respostaCerta = random1 + random2;
+  respostaCerta = controle > 10 ? random1 + random2 : random1 - random2;
+  console.log(respostaCerta)
+
   if (resposta === respostaCerta) {
     answer.className = "respostaCerta";
     newAnswer()
