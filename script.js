148c5af002fc6a6a7517de5b37a3fe388e79fdf5
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-06-20 19:45:38 -0400

    fixed function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function somaSub() {
+function exercicios(location) {
   let nivel = document.getElementById('nivel').value;
   let qtdPerguntas = document.getElementById('qtdPerguntas').value;
-  window.location.href = "src/somaSub.html?" + nivel + "+" + qtdPerguntas;
+  window.location.href = location + nivel + "+" + qtdPerguntas;
 }
