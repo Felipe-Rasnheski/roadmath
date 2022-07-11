43624df4f96d4ee7f9fc0af2247f35bfd3c5c077
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-10 22:01:57 -0400

    add funções

--- src/Contas.js
+++ src/Contas.js
@@ -1,10 +1,13 @@
-class Conta {
-  constructor(max, min) {
-    this.num = parseInt(Math.random() * (max - min) + min)
+export default class Conta {
+  constructor() {
+    
+  }
+
+  qtdPerguntas(qtd) {
+    return parseFloat(qtd.split("").filter(n => (Number(n) || n == 0)).join(""))
+  }
+
+  randomNumber(max, min) {
+    return parseInt(Math.random() * (max - min) + min)
   }
 }
-
-let num1 = new Conta(100, 10)
-let num2 = new Conta(num1.num, 10)
-
-console.log(num1.num, num2.num);
