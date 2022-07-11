d854ba7af1b8e5fe6d79ede219d24c68a32e58c8
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-11 13:39:16 -0400

    Mostra as contas utilizando a class

--- src/somaSub.js
+++ src/somaSub.js
@@ -1,26 +1,17 @@
 import Conta from  "./Contas.js"
-let conta = new Conta()
+let conta = new Conta(window.location.search)
 
 let num1 = document.getElementById("num1")
 let num2 = document.getElementById("num2")
 let max = null
 let min = null
 
-let nivel = window.location.search;
-nivel = nivel.replace("?", "");
-nivel = nivel.replace("+", "");
+document.querySelector("span").textContent = conta.qtdPergun
 
-let qtdPerguntas = conta.qtdPerguntas(nivel)
-
-let controle = qtdPerguntas/2;
-nivel = nivel.replace(qtdPerguntas,'');
-let perguntasQTD = document.querySelector("#qtdPerguntas h3 span");
-perguntasQTD.textContent = qtdPerguntas
-
-if(nivel === "facil") {
+if(conta.nivel === "facil") {
   max = 100;
   min = 30;
-} else if (nivel === "normal") {
+} else if (conta.nivel === "normal") {
   max = 1000;
   min = 100;
 } else {
@@ -28,5 +19,5 @@ if(nivel === "facil") {
   min = 1000;
 }
 
-num1.textContent = conta.randomNumber(100, 10)
-num2.textContent = conta.randomNumber(num1.textContent, 10)
+num1.textContent = conta.randomNumber(max, min)
+num2.textContent = conta.randomNumber(num1.textContent, min)
