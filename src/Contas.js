7f82c20accb9d9a6f3544a33ca82f6ff6b8f9189
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-11 13:41:21 -0400

    melhora class

--- src/Contas.js
+++ src/Contas.js
@@ -1,13 +1,16 @@
 export default class Conta {
-  constructor() {
-    
-  }
 
-  qtdPerguntas(qtd) {
-    return parseFloat(qtd.split("").filter(n => (Number(n) || n == 0)).join(""))
+  constructor(search) {
+    this.nivel = search.replace("?", "").replace("+", "")
+    this.qtdPergun = parseFloat(
+      this.nivel.split("").filter(n => (Number(n) || n == 0)).join("")
+    )
+    this.nivel = this.nivel.replace(this.qtdPergun, "")
+    this.controle = this.qtdPergun / 2
   }
 
   randomNumber(max, min) {
     return parseInt(Math.random() * (max - min) + min)
   }
+  
 }
