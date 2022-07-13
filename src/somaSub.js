49a15b0255749afcbeb1c6f6d0fc76e6d72a93b1
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-12 21:02:13 -0400

    random num

--- src/somaSub.js
+++ src/somaSub.js
@@ -1,17 +1,19 @@
 import Conta from  "./Contas.js"
-let conta = new Conta(window.location.search)
+import Search from "./Search.js"
 
 let num1 = document.getElementById("num1")
 let num2 = document.getElementById("num2")
 let max = null
 let min = null
 
-document.querySelector("span").textContent = conta.qtdPergun
+let search = new Search(window.location.search)
 
-if(conta.nivel === "facil") {
+document.querySelector("span").textContent = ""
+
+if( max === "facil") {
   max = 100;
   min = 30;
-} else if (conta.nivel === "normal") {
+} else if (min === "normal") {
   max = 1000;
   min = 100;
 } else {
@@ -19,5 +21,6 @@ if(conta.nivel === "facil") {
   min = 1000;
 }
 
-num1.textContent = conta.randomNumber(max, min)
-num2.textContent = conta.randomNumber(num1.textContent, min)
+let conta = new Conta(100, 10)
+num1.textContent = conta.random1
+num2.textContent = conta.random2
