2e9d0866a7ac69da1ea4f7cb4947650aafeeae03
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-13 21:11:09 -0400

    answer

--- src/Contas.js
+++ src/Contas.js
@@ -1,9 +1,13 @@
 export default class Conta {
 
-  constructor(max, min) {
+  constructor(max, min, qtdPergun) {
     this.random1 = parseInt(Math.random() * (max - min)) + min
     this.random2 = parseInt(Math.random() * (this.random1 - min)) + min
-    this.answer = this.random1 + this.random2
+    this.answer = setAnswer()
   }
 
+}
+
+function setAnswer() {
+  
 }
