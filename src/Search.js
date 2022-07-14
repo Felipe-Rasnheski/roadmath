66a401b6ea962d0b6d08d04afabe72db10d93cb0
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-13 21:10:32 -0400

    max and min

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,25 @@ export default class Search {
   constructor(search) {
     this.qtdPergun = parseFloat(search.split("").filter(n => (Number(n) || n == 0)).join(""))
     this.nivel = search.replace(this.qtdPergun, "").replace("?", "").replace("+", "")
-    this.controle = this.qtdPergun/2
+    this.max = setMax(this.nivel)
+    this.min = setMin(this.nivel)
+
   }
 
+}
+
+function setMax(nivel) {
+  if(nivel == "facil"){
+    return 100
+  } else if(nivel == "normal") {
+    return 1000
+  } else return 10000
+}
+
+function setMin(nivel) {
+  if(nivel == "facil"){
+    return 10
+  } else if(nivel == "normal") {
+    return 100
+  } else return 1000
 }
