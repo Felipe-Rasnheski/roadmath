edd2f8d4f410fc9ca8dd16834ecab56361fb523f
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-13 21:08:49 -0400

    add conta

--- src/somaSub.js
+++ src/somaSub.js
@@ -3,24 +3,12 @@ import Search from "./Search.js"
 
 let num1 = document.getElementById("num1")
 let num2 = document.getElementById("num2")
-let max = null
-let min = null
 
 let search = new Search(window.location.search)
 
-document.querySelector("span").textContent = ""
+let qtdPerguntas = document.querySelector("span")
+qtdPerguntas.textContent = search.qtdPergun
 
-if( max === "facil") {
-  max = 100;
-  min = 30;
-} else if (min === "normal") {
-  max = 1000;
-  min = 100;
-} else {
-  max = 10000;
-  min = 1000;
-}
-
-let conta = new Conta(100, 10)
+let conta = new Conta(search.max, search.min, search.qtdPergun)
 num1.textContent = conta.random1
 num2.textContent = conta.random2
