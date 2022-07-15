234600f14109f7c44077084030ff7354b6b5a386
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-14 21:12:24 -0400

    checkAnswer

--- src/somaSub.js
+++ src/somaSub.js
@@ -9,6 +9,25 @@ let search = new Search(window.location.search)
 let qtdPerguntas = document.querySelector("span")
 qtdPerguntas.textContent = search.qtdPergun
 
-let conta = new Conta(search.max, search.min, search.qtdPergun)
+let conta = new Conta(search.max, search.min)
 num1.textContent = conta.random1
 num2.textContent = conta.random2
+
+let controle = search.qtdPergun / 2
+
+let resposta = document.getElementById("resposta")
+
+resposta.addEventListener("keydown", event => {
+  if(event.key == "Enter") {
+    checkAnswer()
+  } 
+})
+
+function checkAnswer() {
+  
+  if(resposta.value == conta.answer) {
+    resposta.classList.add("respostaCerta")
+  } else {
+    resposta.classList.add("respostaErrada")
+  }
+}
