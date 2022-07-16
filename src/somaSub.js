018597394546d684ed32b841122bcd2e7cce4303
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-16 19:36:30 -0400

    fixed checkAnswer and class

--- src/somaSub.js
+++ src/somaSub.js
@@ -18,9 +18,10 @@ let controle = search.qtdPergun / 2
 let resposta = document.getElementById("resposta")
 
 resposta.addEventListener("keydown", event => {
+  resposta.classList.remove("respostaErrada")
   if(event.key == "Enter") {
     checkAnswer()
-  } 
+  }
 })
 
 let answer = conta.answer
@@ -40,10 +41,19 @@ function checkAnswer() {
 }
 
 function newConta() {
-  
+
   setTimeout(() => {
+    if(search.qtdPergun == 0) {
+      minutos < 10 ? minutos = "0" + minutos.toString() : minutos
+      segundos < 10 ? segundos = "0" + segundos.toString() : segundos
+      window.location.href = `estatisticas.html?${minutos}:${segundos}`
+    }
     const newConta = new Conta(search.max, search.min)
+    newConta.answerRes = search.qtdPergun
     answer = newConta.answer
+    if(search.qtdPergun == 5) {
+      document.getElementById("acao").innerHTML = "-"
+    }
     resposta.value = ""
     resposta.classList.remove("respostaCerta")
     num1.textContent = newConta.random1
@@ -51,3 +61,16 @@ function newConta() {
   }, 1000)
     
 }
+
+let proximaPergun = document.querySelector(".inner-header button")
+proximaPergun.addEventListener("click", () => checkAnswer())
+
+let segundos = 0
+let minutos = 0
+setInterval(() => {
+  segundos++
+  if (segundos > 60) {
+    minutos++
+    segundos = 0
+  }
+}, 1000);
