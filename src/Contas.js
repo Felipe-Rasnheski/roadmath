86d3f4f4a4a569d93942a39f23c7bd9f5beacadf
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-18 09:18:56 -0400

    ajustes para mult

--- src/Contas.js
+++ src/Contas.js
@@ -4,12 +4,20 @@ export default class Conta {
     this.random1 = parseInt(Math.random() * (max - min)) + min
     this.random2 = parseInt(Math.random() * (this.random1 - min)) + min
     this.answer = this.random1 + this.random2
+    this.mult = this.random1 * this.random2
   }
 
-  set answerRes(qtdPergun) {
+  set answerSoma(qtdPergun) {
     if(qtdPergun <= 5) {
       this.answer = this.random1 - this.random2
-    } else return
+    }
   }
 
+  set answerMult(qtdPergun) {
+    if(qtdPergun <= 5) {
+      this.answer = this.random2
+    } else {
+      this.answer = this.mult
+    }
+  }
 }
