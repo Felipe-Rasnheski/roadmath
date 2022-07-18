690a370a58d77cec5a17227e269f3c17ef3d7a3c
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-18 10:10:43 -0400

    add Mult

--- src/Contas.js
+++ src/Contas.js
@@ -1,6 +1,7 @@
 export default class Conta {
 
-  constructor(max, min) {
+  constructor(max, min, controle) {
+    this.controle = controle
     this.random1 = parseInt(Math.random() * (max - min)) + min
     this.random2 = parseInt(Math.random() * (this.random1 - min)) + min
     this.answer = this.random1 + this.random2
@@ -8,13 +9,13 @@ export default class Conta {
   }
 
   set answerSoma(qtdPergun) {
-    if(qtdPergun <= 5) {
+    if(qtdPergun <= this.controle) {
       this.answer = this.random1 - this.random2
     }
   }
 
   set answerMult(qtdPergun) {
-    if(qtdPergun <= 5) {
+    if(qtdPergun <= this.controle) {
       this.answer = this.random2
     } else {
       this.answer = this.mult
